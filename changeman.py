fc560a6a98c7b7e3e8467ae08fa8a98d7b1a784d
Saju159 <92033581+Saju159@users.noreply.github.com>
2026-07-29 10:50:18 +0300

    improved main menu logic and added changeman

---
+++ changeman.py
@@ -0,0 +1,9 @@
+import func
+
+
+def run():
+    func.clear()
+    func.delay()
+    print("ChangeMan Change log viewer.")
+    print("\nNothing here. What did you expect? Something usefull?")
+    input("Press ENTER to exit.")
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import viewer
 import func
 import userman
 import choreman
+import changeman
 
 
 while True:
@@ -14,25 +15,20 @@ while True:
     print("Welcome to SCMS (Saju's Chore Management System.)")
     print("Please enter a valid option: \n1. View Your Chores.\n2. Create or edit a chore.\n3. Create/edit users. \n9. View Change log. \n(1=Default).")
     option=input("Option: ")
-    try:
-        option=int(option)
-    except Exception:
-        print("Option is not a valid number.")
-        if option=="":
-            print("Using default value.")
-            option=1
-        else:
-            print("Invalid option. Try again.")
 
-    print(option)
-
-    if option==1:
+    if option=="1":
         viewer.run()
 
-    elif option==2:
+    elif option=="2":
         choreman.validate()
 
-    elif option==3:
+    elif option=="3":
         userman.run()
+
+    elif option=="9":
+        changeman.run()
+
+    else:
+        print("Invalid option. Try again.")
 
     func.delay()
